c4fd8ee2875587a181ddb7c6d145b69dab2732cf
zovl <849075906@qq.com>
2016-11-02 14:11:59 +0800

    2016-11-2 14:11:56

---
+++ mina-demo/src/zovlzhongguanhua/demo/mina/server/MINAServer.java
@@ -0,0 +1,66 @@
+package zovlzhongguanhua.demo.mina.server;
+
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.example.echoserver.EchoProtocolHandler;
+import org.apache.mina.transport.socket.SocketAcceptor;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * MINA服务器
+ */
+public class MINAServer {
+
+    public static void main(String args[]) {
+
+        SocketAcceptor acceptor = new NioSocketAcceptor();
+        acceptor.setReuseAddress( true );
+        acceptor.setHandler(new EchoProtocolHandler() {
+
+            @Override
+            public void sessionCreated(IoSession session) {
+                super.sessionCreated(session);
+                System.out.println("sessionCreated: session=" + session);
+            }
+
+            @Override
+            public void sessionClosed(IoSession session) throws Exception {
+                super.sessionClosed(session);
+                System.out.println("sessionClosed: session=" + session);
+            }
+
+            @Override
+            public void sessionOpened(IoSession session) throws Exception {
+                super.sessionOpened(session);
+                System.out.println("sessionOpened: session=" + session);
+            }
+
+            @Override
+            public void messageReceived(IoSession session, Object message) throws Exception {
+                super.messageReceived(session, message);
+                System.out.println("messageReceived: session=" + session);
+            }
+
+            @Override
+            public void messageSent(IoSession session, Object message) throws Exception {
+                super.messageSent(session, message);
+                System.out.println("messageSent: message=" + message);
+            }
+        });
+        try {
+            acceptor.bind(new InetSocketAddress(4722));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (;;) {
+            System.out.println("R: " + acceptor.getStatistics().getReadBytesThroughput() + ", W: " + acceptor.getStatistics().getWrittenBytesThroughput());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ socket-demo/src/zovlzhongguanhua/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by zovl on 28/10/2016.
+ */
+package zovlzhongguanhua.demo;
