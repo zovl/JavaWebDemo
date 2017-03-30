898fe71efcae31ce5687fb6b6c8d7e65baf112ce
849075906@qq.com <zz880326>
2017-03-30 09:20:21 +0800

    2017-3-30 09:20:09

---
+++ apache-commons-demo/src/zovlzhongguanhua/demo/commons/Codec.java
@@ -0,0 +1,104 @@
+package zovlzhongguanhua.demo.commons;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.*;
+
+import java.math.BigInteger;
+
+public class Codec {
+
+    public static void main(String[] args) {
+
+        String string = "abc";
+        byte[] bytes = string.getBytes();
+        BigInteger bigInteger = new BigInteger("123456789123456789123456789123456789");
+
+        /*信息摘要*/
+
+        digest(string);
+        hmac(bytes);
+        md5(bytes);
+        sha(bytes);
+        unix(string, bytes);
+
+        /*信息编解码*/
+
+        base64(bytes, bigInteger);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private static void digest(String string) {
+        System.out.println(DigestUtils.md2(string));
+        System.out.println(DigestUtils.md2Hex(string));
+
+        System.out.println(DigestUtils.md5(string));
+        System.out.println(DigestUtils.md5Hex(string));
+
+        System.out.println(DigestUtils.sha(string));
+        System.out.println(DigestUtils.shaHex(string));
+
+        System.out.println(DigestUtils.sha1(string));
+        System.out.println(DigestUtils.sha1Hex(string));
+
+        System.out.println(DigestUtils.sha256(string));
+        System.out.println(DigestUtils.sha256Hex(string));
+
+        System.out.println(DigestUtils.sha384(string));
+        System.out.println(DigestUtils.sha384Hex(string));
+
+        System.out.println(DigestUtils.sha512(string));
+        System.out.println(DigestUtils.sha512Hex(string));
+    }
+
+    private static void hmac(byte[] bytes) {
+        System.out.println(HmacUtils.getHmacMd5(bytes));
+        System.out.println(HmacUtils.getHmacSha1(bytes));
+        System.out.println(HmacUtils.getHmacSha256(bytes));
+        System.out.println(HmacUtils.getHmacSha384(bytes));
+        System.out.println(HmacUtils.getHmacSha512(bytes));
+    }
+
+    private static void md5(byte[] bytes) {
+        System.out.println(Md5Crypt.apr1Crypt(bytes));
+        System.out.println(Md5Crypt.md5Crypt(bytes));
+    }
+
+    private static void sha(byte[] bytes) {
+        System.out.println(Sha2Crypt.sha256Crypt(bytes));
+        System.out.println(Sha2Crypt.sha512Crypt(bytes));
+    }
+
+    private static void unix(String string, byte[] bytes) {
+        System.out.println(UnixCrypt.crypt(bytes));
+        System.out.println(UnixCrypt.crypt(string));
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private static void base64(byte[] bytes, BigInteger bigInteger) {
+        byte[] encodeBytes = Base64.encodeBase64(bytes);
+        if (Base64.isBase64(encodeBytes)) {
+            byte[] decodeBytes = Base64.decodeBase64(encodeBytes);
+            System.out.println(String.valueOf(decodeBytes));
+        }
+
+        byte[] encodeInt = Base64.encodeInteger(bigInteger);
+        if (Base64.isBase64(encodeInt)) {
+            BigInteger decodeInt = Base64.decodeInteger(encodeInt);
+            System.out.println(decodeInt);
+        }
+
+        String encodeString = Base64.encodeBase64String(bytes);
+        if (Base64.isBase64(encodeString)) {
+            byte[] decodeString = Base64.decodeBase64(encodeString);
+            System.out.println(String.valueOf(decodeString));
+        }
+    }
+}
---
+++ unirest-demo/src/zovlzhongguanhua/demo/commons/Main.java
@@ -0,0 +1,177 @@
+package zovlzhongguanhua.demo.unirest;
+
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.BaseRequest;
+import com.mashape.unirest.request.HttpRequest;
+import com.mashape.unirest.request.body.Body;
+import org.apache.http.HttpEntity;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        get();
+        // post();
+        // upload();
+        // custom();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private static void post() {
+        HttpResponse<String> response = null;
+        BaseRequest request = Unirest.post("http://httpbin.org/post")
+                .queryString("name", "Mark")
+                .field("last", "Polo");
+        try {
+            response = request.asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        httpRequest(request.getHttpRequest());
+        httpResponse(response);
+    }
+
+    private static void get() {
+        HttpResponse<String> response = null;
+        BaseRequest request = Unirest.get("http://httpbin.org/{method}")
+                .routeParam("method", "get")
+                .queryString("name", "Mark");
+
+        try {
+            response = request.asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        httpRequest(request.getHttpRequest());
+        httpResponse(response);
+    }
+
+    private static void upload() {
+        HttpResponse<String> response = null;
+        BaseRequest request = Unirest.post("http://httpbin.org/post")
+                .header("accept", "application/json")
+                .field("parameter", "value")
+                .field("file", new File(System.getProperty("user.dir") + File.separator + "unirest-demo" + File.separator + "uploadFile"));
+
+        try {
+            response = request.asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        httpRequest(request.getHttpRequest());
+        httpResponse(response);
+    }
+
+    private static void custom() {
+        String userDir = System.getProperty("user.dir");
+        HttpResponse<String> response = null;
+        BaseRequest request = Unirest.post("http://httpbin.org/post")
+                .header("accept", "application/json")
+                .body("{\"parameter\":\"value\", \"foo\":\"bar\"}");
+
+        try {
+            response = request.asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        httpRequest(request.getHttpRequest());
+        httpResponse(response);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public static void httpRequest(HttpRequest request) {
+        System.out.println("-------------------->");
+
+        System.out.println("url：" + request.getUrl());
+
+        HttpMethod method = request.getHttpMethod();
+        if (method != null) {
+            System.out.println("method：" + method.name());
+            System.out.println("method：" + method.ordinal());
+        }
+
+        Body body = request.getBody();
+        if (body != null) {
+            System.out.println("body：" + body.toString());
+        }
+
+        if (body != null) {
+            HttpEntity entity = body.getEntity();
+            if (entity != null) {
+                System.out.println("isChunked：" + entity.isChunked());
+                System.out.println("isRepeatable：" + entity.isRepeatable());
+                System.out.println("contentLength：" + entity.getContentLength());
+                System.out.println("isStreaming：" + entity.isStreaming());
+            }
+        }
+
+        Map<String, List<String>> headers = request.getHeaders();
+        if (headers != null) {
+            Set<String> keys = headers.keySet();
+            for (String key : keys) {
+                List<String> values = headers.get(key);
+                StringBuffer buffer = new StringBuffer();
+                int i = 0;
+                for (String value : values) {
+                    ++i;
+                    if (i == values.size()) {
+                        buffer.append(value);
+                    } else {
+                        buffer.append(value + "/");
+                    }
+                }
+                System.out.println("header：" + key + "=" + buffer);
+            }
+        }
+
+        // System.out.println("contentType：" + entity.getContentType().getName() + "=" + entity.getContentType().getValue());
+        // System.out.println("contentEncoding：" + entity.getContentEncoding().getName() + "=" + entity.getContentEncoding().getValue());
+    }
+
+    public static void httpResponse(HttpResponse response) {
+        System.out.println("-------------------->");
+
+        System.out.println("body：" + response.getBody());
+        System.out.println("statusText：" + response.getStatusText());
+        System.out.println("status：" + response.getStatus());
+        System.out.println("rawBody：" + response.getRawBody());
+        System.out.println("rawBody：" + response.getRawBody().toString());
+
+        Headers headers = response.getHeaders();
+        if (headers != null) {
+            Set<String> keys = headers.keySet();
+            for (String key : keys) {
+                List<String> values = headers.get(key);
+                StringBuffer buffer = new StringBuffer();
+                int i = 0;
+                for (Object value : values) {
+                    ++i;
+                    if (i == values.size()) {
+                        buffer.append(value);
+                    } else {
+                        buffer.append(value + "/");
+                    }
+                }
+                System.out.println("header：" + key + "=" + buffer);
+            }
+        }
+    }
+}
